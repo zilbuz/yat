c5e263f54a653825443f3b6e43dab64db893a613
Basile Desloges <basile.desloges@gmail.com>
2011-02-01 02:35:17 -0500

    Implemeted add command
    The add command parse the command arguments and add the task (or tag, or list)
    to the database.
    
    Also added a default priority in the config file, and a stub list command which
    print the database content.

--- yat/yat.py
+++ yat/yat.py
@@ -9,6 +9,8 @@ import inspect
 import locale
 import os
 import re
+import sqlite3
+import string
 import sys
 
 class Command:
@@ -94,16 +96,164 @@ the name of a command is provided, show the specific help text for this command.
 class AddCommand (Command):
     u"""Add a task, a list or a tag
 
-usage: %s add [task | list | tag] <informations>
+usage: %s add [task | list | tag] "<informations>"
+
+Add a task, a list or a tag to the todolist, depending of the first argument
+after "add". If the first argument after "add" isn't one of these, then "task"
+is assumed.
+
+Adding a task:
+    When adding a task, you must provide the task's text, and optionnally some
+    symbols to set the priority, due date, tags or list. The symbols are '*',
+    '^', '#' and '>'.
+
+    '*' can be used to set the priority of the task, it must be followed by a number
+    between 0 and 3. 
+    Example: yat add "do the laundry *2".
+
+    '^' can be used to set the due date of the task, it must be followed by a
+    date of the form dd/mm/yyyy. 
+    Example: yat add "go to the cinema with Wendy ^12/02/2011".
+
+    '#' can be used to set the tags of the task, it must be followed by the name
+    of a tag. The tag name can be composed of anything but spaces. You can set 
+    multiple tags for a task. If the tag doesn't exist, it will be created.
+    Example: yat add "do my homework #work #important"
+
+    '>' can be used to set the list of the task, it must be followed by the name
+    of a list. The list name can be composed of anything but spaces. If the list
+    doesn't exist, it will be created.
+    Example: yad add "peanut butter >groceries"
+
+    Apart for the tags, if a symbol is used more than one time for a task, the
+    last one will be used.
+    You can place the symbols anywhere in the task text.
+    You can escape the symbols with \, for example: "the symbol \> won't be
+    processed with this text". And of course, you also have to escape \ if you
+    want to use it.
+    If your task text uses none of the symbols that the shell can interpret, the
+    double quotes are optionnal.
+
+Adding a list:
+    Just add a new list. The name can contain anything but spaces.
+    Example: yat add list groceries
+
+Adding a tag:
+    Just add a new tag. The name can contain anything but spaces.
+    Example: yat add tag work
 """
     
     alias = [u"add"]
 
+    def __init__(self):
+        self.re_priority = re.compile("^\*(\d)$")
+        u"""Regex for the priorty"""
+
+        self.re_tag = re.compile("^#(.*)$")
+        u"""Regex for the tags"""
+
+        self.re_list = re.compile("^>(.*)$")
+        u"""Regex for the list"""
+
+        self.re_date = re.compile("^\^(\d\d/\d\d/\d{4})$")
+        u"""Regex for the date"""
+
     def execute(self,args):
-        # TODO
-        print self.__doc__.split('\n',1)[0]," ",args
+        global sql, config
+        # Separate words
+        args = (" ".join(args)).split(" ")
+
+        cmd = ""
+        if args[0] in ["task", "list", "tag"]:
+            cmd = args[0]
+            args = args[1:]
+
+        # Process command
+        if cmd in ["tag", "list"]:
+            self.__add_tag_or_list(cmd + "s", args[0])
+            pass
+        else:
+            # Init params
+            priority = ""
+            tags = []
+            list = ""
+            date = ""
+            text = []
+            # Look for symbols
+            for a in args:
+                symbol = False
+                # Priority
+                res = self.re_priority.match(a)
+                if res != None:
+                    priority = int(res.group(1))
+                    symbol = True
+                # Tags
+                res = self.re_tag.match(a)
+                if res != None:
+                    tags.append(res.group(1))
+                    symbol = True
+                # List
+                res = self.re_list.match(a)
+                if res != None:
+                    list = res.group(1)
+                    symbol = True
+                # Date
+                res = self.re_date.match(a)
+                if res != None:
+                    date = res.group(1)
+                    symbol = True
+
+                # Regular text
+                if not symbol:
+                    text.append(a)
+
+            if priority == "":
+                priority = config["default_priority"]
+            elif priority > 3:
+                priority = 3
+
+            if tags == []:
+                tags.append(config["default_tag"])
+            tags_copy = []
+            for t in tags:
+                self.__add_tag_or_list("tags", t)
+                tags_copy.append(self.__get_id("tags", t))
+            tags = ",".join(tags_copy)
+
+            if list == "":
+                list = config["default_list"]
+            self.__add_tag_or_list("lists", list)
+            list = self.__get_id("lists", list)
+
+            text = " ".join(text)
+
+            # Add the task with the correct parameters
+            with sql:
+                sql.execute('insert into tasks values(null, ?, ?, ?, ?, ?)',
+                        (text, priority, date, tags, list))
+            pass
         pass
+
+    def __add_tag_or_list(self, table, name):
+        u"""Add an element "name" to the "table" if it doesn't exist. It is
+        meant to be used with table="lists" or table="tags" """
+        global sql
+        with sql:
+            c = sql.execute('select count(*) as nb from %s where name=?' %
+                    table, (name,))
+            if c.fetchone()[0] == 0:
+                sql.execute('insert into %s values(null, ?)' % table, (name,))
+                sql.commit()
+
+    def __get_id(self, table, name):
+        u"""Get the id of the element "name" in "table". It's meant to be used
+        with table = "lists" or "tags" """
+        global sql
+        with sql:
+            res = sql.execute('select id from %s where name=?' % table, (name,))
+            return str(res.fetchone()[0])
     pass
+    
 
 class RemoveCommand (Command):
     u"""Remove a task, a list or a tag
@@ -128,8 +278,18 @@ usage: %s list
     alias = [u"list", u"show"]
 
     def execute(self,args):
+        global sql
         # TODO
-        print self.__doc__.split('\n',1)[0]," ",args
+        with sql:
+            print "tasks:"
+            for r in sql.execute("""select * from tasks"""):
+                print "\t",r
+            print "lists:"
+            for r in sql.execute("""select * from lists"""):
+                print "\t",r
+            print "tags:"
+            for r in sql.execute("""select * from tags"""):
+                print "\t",r
         pass
     pass
 
@@ -140,7 +300,7 @@ def init():
     - initialising database access
     - initializing some global vars
     """
-    global enc, out, config
+    global enc, out, config, sql
 
     # Default encoding
     enc = locale.getpreferredencoding()
@@ -167,12 +327,44 @@ def init():
     config["yatdir"] = config.get("yatdir", "~/.yat")
     config["default_list"] = config.get("default_list", "nolist")
     config["default_tag"] = config.get("default_tag", "notag")
+    config["default_priority"] = config.get("default_priority", "1")
 
     # Create yat directory
     if config["yatdir"][0] == "~":
         config["yatdir"] = os.environ.get("HOME") + config["yatdir"][1:]
     if not os.path.isdir(config["yatdir"]):
         os.makedirs(config["yatdir"], mode=0700)
+
+    # Connect to sqlite db
+    sql = sqlite3.connect(config["yatdir"] + "/yat.db")
+    try:
+        with sql:
+            sql.execute("select * from tags")
+    except sqlite3.OperationalError:
+        # Create tables
+        with sql:
+            sql.execute("""
+                create table tasks (
+                    id integer primary key,
+                    task text,
+                    priority int,
+                    due_date text,
+                    tags text,
+                    list text
+                )""")
+            sql.execute("""
+                create table tags (
+                    id integer primary key,
+                    name text
+                    )""")
+            sql.execute("""
+                create table lists (
+                    id integer primary key,
+                    name text
+                    )""")
+            sql.execute("""insert into tags values (null, "notag")""")
+            sql.execute("""insert into lists values (null, "nolist")""")
+            sql.commit()
     pass
 
 def isCommand(obj):
