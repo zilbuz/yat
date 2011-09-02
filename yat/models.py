8778d1f811fe17d84378eb646f94332886dc16be
Simon Chopin <chopin.simon@googlemail.com>
2011-09-02 19:44:01 +0200

    yat.models: pylintification.

--- yat/models.py
+++ yat/models.py
@@ -28,10 +28,13 @@ http://sam.zoy.org/wtfpl/COPYING for more details.
 
 from yat.exceptions import WrongId, IncoherentObject, WrongName
 
+#pylint: disable=R0903
 class DBObject(object):
+    u"""Describes the basis of all the objects having a DB representation."""
     class_lib = None
 
     def __setattr__(self, attr, value):
+        u'''When changing an attribute, memorize it.'''
         super(DBObject, self).__setattr__('changed', True)
         super(DBObject, self).__setattr__(attr, value)
 
@@ -42,12 +45,16 @@ class DBObject(object):
         elif self.lib == None:
             self.lib = DBObject.class_lib
 
+        #pylint: disable=C0103
         self.id = None
+        #pylint: enable=C0103
         self.content = None
         self.created = 0
         self.hash = None
         self.last_modified = 0
+#pylint: enable=R0903
 
+#pylint: disable=R0902
 class Task(DBObject):
     u'''This class describe a task in a OO perspective
     '''
@@ -57,13 +64,14 @@ class Task(DBObject):
         """
         super(Task, self).__init__(lib)
         # Creation of a blank Task
-        self.parent=None
-        self.due_date=None
-        self.priority=None
+        self.parent = None
+        self.due_date = None
+        self.priority = None
         self.list = NoList(self.lib)
         self.tags = set()
         self.completed = 0
         self.changed = False
+        self.notes = []
 
     def check_values(self, lib = None):
         u'''Checks the values of the object, and correct them
@@ -79,8 +87,8 @@ class Task(DBObject):
         elif self.priority > 3:
             self.priority = 3
 
-        for t in self.tags:
-            t.check_values(lib)
+        for tag in self.tags:
+            tag.check_values(lib)
 
         self.list.check_values(lib)
 
@@ -109,31 +117,37 @@ The return value is always None.'''
         if lib == None:
             lib = self.lib
         elif lib != self.lib:
+            #pylint: disable=C0103
             self.id = None
+            #pylint: enable=C0103
 
-        rv = self.list.save(lib)
-        if rv != None:  # The list has been replaced in the new lib
-            self.list = rv
+        new_list = self.list.save(lib)
+        if new_list != None:  # The list has been replaced in the new lib
+            self.list = new_list
 
         to_remove = set()
         to_add = set()
-        for t in self.tags:
-            rv = t.save(lib)
-            if rv != None:  # The tag has been replaced in the new lib
-                to_add.add(rv)
-                to_remove.add(t)
+        for tag in self.tags:
+            new_tag = tag.save(lib)
+            if new_tag != None:  # The tag has been replaced in the new lib
+                to_add.add(new_tag)
+                to_remove.add(new_tag)
         self.tags -= to_remove
         self.tags |= to_add
 
         if self.parent != None:
-            rv = self.parent.save(lib)
-            if rv != None:  # It shouldn't happen, but better safe than sorry
-                self.parent = rv
-
+            new_parent = self.parent.save(lib)
+            # It shouldn't happen, but better safe than sorry
+            if new_parent != None:
+                self.parent = new_parent
+        #pylint: disable=W0212
+        # The access to the protected functions is deliberate,
+        # it was meant like that.
         if self.id == None:
             save_function = lib._add_task
         else:
             save_function = lib._edit_task
+        #pylint: enable=W0212
         save_function(self)
         self.lib = lib
         return None
@@ -155,11 +169,13 @@ The return value is always None.'''
         root ancestor."""
         if self.parent == None:
             return []
-        list = self.parent.get_list_parents()
-        list.append(self.parent)
-        return list
+        parent_list = self.parent.get_list_parents()
+        parent_list.append(self.parent)
+        return parent_list
+#pylint: enable=R0902
 
 class Note(DBObject):
+    u'''The python representation of a task annotation.'''
     def __init__(self, lib):
         super(Note, self).__init__(lib)
         self.task = None
@@ -174,14 +190,18 @@ class Note(DBObject):
             try:
                 return lib.get_note(self.task, self.content, False)
             except WrongName:
+                #pylint: disable=C0103
                 self.id = None
+                #pylint: enable=C0103
             except WrongId:
                 raise IncoherentObject('A note must be associated to \
                                         an existing task.')
+        #pylint: disable=W0212
         if self.id == None:
             lib._add_note(self)
         elif self.changed:
             lib._edit_note(self)
+        #pylint: enable=W0212
         self.lib = lib
         return None
 
@@ -195,7 +215,9 @@ class Note(DBObject):
         if self.created <= 0:
             self.created = self.lib.get_time()
 
+#pylint: disable=R0903
 class Group(DBObject):
+    u'''Abstraction for the tags and the lists.'''
     def __init__(self, lib):
         u"""Constructs a group from an sql row"""
         super(Group, self).__init__(lib)
@@ -222,21 +244,26 @@ class Group(DBObject):
             try:
                 return getter(self.content, False)
             except WrongName:
+                #pylint: disable=C0103
                 self.id = None
+                #pylint: enable=C0103
+        #pylint: disable=W0212
         if self.id == None:
-            lib._add_group(self._table_name, self)
+            lib._add_group(self)
         elif self.changed:
-            lib._edit_group(self._table_name, self)
+            lib._edit_group(self)
+        #pylint: enable=W0212
         self.lib = lib
         return None
+#pylint: enable=R0903
 
 class List(Group):
     u'''This class represent the "meta-data" of a list.
     It is NOT a container !'''
-    _table_name = 'lists'
+    table_name = 'lists'
 
     def __init__(self, lib):
-        u"""Constructs a List from an sql entry."""
+        u"""Constructs an empty List."""
         super(List, self).__init__(lib)
 
     def __str__(self):
@@ -244,6 +271,8 @@ class List(Group):
         return retour
 
     def save(self, lib=None):
+        u'''Saves the Tag instance into the DB represented by lib, or
+        the one referenced by the instance.'''
         if lib == None:
             lib = self.lib
         return self._save(lib, lib.get_list)
@@ -251,31 +280,40 @@ class List(Group):
 class Tag(Group):
     u'''This class represent the "meta-data" of a tag.
     It is NOT a container !'''
-    _table_name = 'tags'
+    table_name = 'tags'
 
-    def __init__(self, lib, sql_line = None):
-        u"""Constructs a Tag from an sql entry."""
+    def __init__(self, lib):
+        u"""Constructs an empty Tag."""
         super(Tag, self).__init__(lib)
 
     def save(self, lib=None):
+        u'''Saves the Tag instance into the DB represented by lib, or
+        the one referenced by the instance.'''
         if lib == None:
             lib = self.lib
         return self._save(lib, lib.get_tag)
 
+#pylint: disable=W0613
 class NoGroup(object):
+    u'''Bogus class used to represent the absence of groups.'''
     def __init__(self, lib):
+        #pylint: disable=C0103
         self.id = None
+        #pylint: enable=C0103
         self.lib = lib
 
     def save(self, lib = None):
+        u'''Bogus function.'''
         pass
 
     def check_values(self, lib = None):
+        u'''Bogus function.'''
         pass
 
 class NoList(NoGroup, List):
+    u'''Bogus specialization for the List class counterpart.'''
     # List provides unique algorithms, but here we override its
-    #polymorphism abilities
+    # polymorphism abilities
     __mro__ = (NoGroup, Group, DBObject, object, List)
     __instance = None
     def __new__(cls, lib):
@@ -287,6 +325,7 @@ class NoList(NoGroup, List):
         super(NoList, self).__init__(lib)
 
 class NoTag(NoGroup, Tag):
+    u'''Bogus specialization for the Tag class counterpart.'''
     # Same as for NoList's MRO.
     __mro__ = (NoGroup, Group, object, Tag)
     __instance = None
@@ -298,4 +337,4 @@ class NoTag(NoGroup, Tag):
 
     def __init__(self, lib):
         super(NoTag, self).__init__(lib)
-
+#pylint: enable=W0613
